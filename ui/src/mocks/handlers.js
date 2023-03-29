2c396c4179c46e57bebeac6f1d17a09661f80177
Addie Pasok <addiepasok8@gmail.com>
2023-03-28 22:38:30 -0500

    commented out handler

--- ui/src/mocks/handlers.js
+++ ui/src/mocks/handlers.js
@@ -7,10 +7,10 @@ import { rest } from 'msw'
 const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:9000'
 
 export const handlers = [
-  rest.post('/auth/login', (req, res, ctx) => {
-    return res(
-       ctx.status(200),
-       ctx.json({token: 'thisisajsonwebtoken'})
-    )
- }),
+//   rest.post('/auth/login', (req, res, ctx) => {
+//     return res(
+//        ctx.status(200),
+//        ctx.json({token: 'thisisajsonwebtoken'})
+//     )
+//  }),
 ]
