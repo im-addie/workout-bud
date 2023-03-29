ebb2185a3eb92a18858101c3d08bf226bb6ce8af
Addie Pasok <addiepasok8@gmail.com>
2023-03-28 22:04:01 -0500

    changed "user" to email

--- api/src/controller/auth.js
+++ api/src/controller/auth.js
@@ -1,4 +1,4 @@
-const { showUserByUsername  } = require('../service/user')
+const { showUserByEmail  } = require('../service/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
@@ -9,16 +9,16 @@ exports.login = async (req, res) => {
   }
 
   const credentials = Buffer.from(authHeader.slice(6), 'base64').toString().split(':')
-  const [username, password] = credentials
+  const [email, password] = credentials
 
-  const user = await showUserByUsername(username)
+  const user = await showUserByEmail(email)
 
   // If the user isn't found or the password is incorrect, return an error
-  if (!user || !await bcrypt.compare(password, user.password)) {
-    return res.status(401).json({ message: 'Invalid username or password' })
+  if (!user || !await bcrypt.compare(password, users.password)) {
+    return res.status(401).json({ message: 'Invalid email or password' })
   }
 
   // Create a JWT and send it back to the client
-  const token = jwt.sign({ id: user.user_id }, process.env.SECRET_KEY)
+  const token = jwt.sign({ id: users.email }, process.env.SECRET_KEY)
   res.json({ token })
 }
