60042966794ffccf5b69d061493cdf02387bf3f1
Addie Pasok <addiepasok8@gmail.com>
2023-03-28 22:38:19 -0500

    refactored code

--- api/src/service/user.js
+++ api/src/service/user.js
@@ -2,6 +2,6 @@ const knex = require('../knex')
 
 exports.showUserByEmail= async (email) => {
   // Find the first user in the database with the email
-const user = await knex('users').where('email', email).first()
-return user
+  const user = await knex('users').where('email', email).first()
+  return user
 }
