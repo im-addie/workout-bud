e59522f4f1517d801778577cf463630263eeb927
Addie Pasok <addiepasok8@gmail.com>
2023-04-04 22:01:56 -0400

    FEAT: dashboard page

--- ui/src/views/Dashboard/index.js
+++ ui/src/views/Dashboard/index.js
@@ -1,62 +1,60 @@
 import React from 'react'
-import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import { Typography } from '@mui/material'
 import Grid from '@mui/material/Grid'
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-
-const loggedExercises = [
-  {
-    id: 1,
-    workout_id: 1,
-    name: 'T-Bar row',
-    weight: '90 lbs',
-    reps: 12
-  },
-  {
-    id: 2,
-    workout_id: 1,
-    name: 'Dumbbell curls',
-    weight: '20 lbs',
-    reps: 15
-  },
-  {
-    id: 3,
-    workout_id: 1,
-    name: 'Lat pulldown',
-    weight: '100 lbs',
-    reps: 12
-  },
-  {
-    id: 4,
-    workout_id: 1,
-    name: 'Hammer curls',
-    weight: '15 lbs',
-    reps: 12
-  }
-]
-
-const workouts = [
-  {
-    id: 1,
-    name: 'Back and Biceps Day',
-    date: 'March 30, 2023'
-  },
-  {
-    id: 2,
-    name: 'Chest and Triceps Day',
-    date: 'March 29, 2023'
-  }
-]
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import { isUserLoggedIn, getToken } from '../../utility/utils'
+import { getUser } from '../../utility/api'
 
 function Dashboard() {
+  const [workoutData, setWorkoutData] = useState([])
+  const [user, setUser] = useState(null);
+
+  // Get user data from API
+  useEffect(() => {
+    // check if the user is logged in
+    if (isUserLoggedIn()) {
+      // get token
+      const token = getToken()
+      // fetch user's data
+      getUser(token)
+        .then((data) => setUser(data))
+        
+        .catch((error) => console.log(error))
+    }
+  
+  }, [])
+
+  useEffect(() => {
+    fetch(`http://localhost:9000/${user?.id}/workouts`)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error("Failed to fetch")
+        }
+        return response.json() // parse the response data
+      })
+      .then(result => setWorkoutData(result)) // set state when the data received
+      .catch(err => err) // return the error
+  }, [user])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(!isUserLoggedIn()) {
+      navigate('/login')
+    }
+
+  }, [])
+
   return (
     <Grid
       container
@@ -64,10 +62,9 @@ function Dashboard() {
       justifyContent="flex-start"
       alignItems="flex-start"
       mt='50px'
-      ml='125px'
     >
       
-      <Grid item xs={6}>
+      <Grid item xs={6} ml='125px'>
         <Typography variant='h4' fontWeight='bold'>
           RECENT ACTIVITY
         </Typography>
@@ -81,43 +78,43 @@ function Dashboard() {
         </Button>
       </Grid>
     
-      <Grid item xs={7} mt='15px'>
+      <Grid item xs={7} mt='15px' ml='125px'>
 
-        {workouts.map((workout) => (
+        {workoutData.map((workout) => (
         <Card sx={{borderRadius: '15px', boxShadow:'3px 2px 7px rgb(0, 0, 0, 0.3)', mb: '45px'}}>
           <CardContent>
             
             <Typography variant='h6' fontWeight='bold'>
-              {workout.name}
+              {workout.loggedExercises[0].muscleGroup}
             </Typography>
 
             <Typography>
-              {workout.date}
+              {workout.dateOfWorkout}
             </Typography>
 
             <TableContainer>
               <Table>
                 <TableHead>
                   <TableRow>
-                    <TableCell component='Typography' sx={{fontWeight: 'bold'}}>Exercise</TableCell>
-                    <TableCell component='Typography' sx={{fontWeight: 'bold'}}>Weight</TableCell>
-                    <TableCell component='Typography' sx={{fontWeight: 'bold'}}>Reps</TableCell>
+                    <TableCell sx={{fontWeight: 'bold'}}>Exercise</TableCell>
+                    <TableCell sx={{fontWeight: 'bold'}}>Weight</TableCell>
+                    <TableCell sx={{fontWeight: 'bold'}}>Reps</TableCell>
                   </TableRow>
                 </TableHead>
 
                 <TableBody>
-                  {loggedExercises.map((exercise) => (
+                  {workout.loggedExercises.map((exercise) => (
                     <TableRow
                       key={exercise.name}
                     >
-                      <TableCell component="th" scope="row">
-                        {exercise.name}
-                      </TableCell>
+                      <TableCell component="th" scope="row" width='350px'>{exercise.name}</TableCell> 
                       <TableCell>{exercise.weight}</TableCell>
                       <TableCell>{exercise.reps}</TableCell>
+                    
                     </TableRow>
                   ))}
                 </TableBody>
+
               </Table>
             </TableContainer>
           
