53eea328fcd50df04f802f7d7bfe536b885b1e79
Addie Pasok <addiepasok8@gmail.com>
2023-04-06 16:42:01 -0400

    added styling

--- ui/src/layouts/Footer.js
+++ ui/src/layouts/Footer.js
@@ -1,21 +1,35 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import { Link } from "react-router-dom";
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ArchiveIcon from '@mui/icons-material/Archive';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import Paper from '@mui/material/Paper';
 
 const Footer = () => {
   return (
     <Box mt='50px'>
       <Paper sx={{ position: 'sticky', bottom: 0, left: 0, right: 0, mt: 'auto' }} elevation={3}>
-        
+
         <BottomNavigation>
-          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
+
+          <Grid sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', gap: '8px' }}>
+
+            <FitnessCenterIcon />
+
+            <Typography
+              sx={{
+                fontFamily: 'impact',
+                letterSpacing: '.1rem',
+                fontSize: '26px',
+              }}
+            >
+              WORKOUT BUD
+            </Typography>
+
+          </Grid>
+
         </BottomNavigation>
 
       </Paper>
