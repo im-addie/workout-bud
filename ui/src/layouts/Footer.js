d8c6daf2dc98db937d10dcd17a379f25dcb7c83a
Addie <121542304+im-addie@users.noreply.github.com>
2023-04-06 16:47:41 -0400

    Revert "FEAT: footer and fixed layout (#35)"
    This reverts commit c56256d43d4f075390b908933fbf050b87866101.

--- ui/src/layouts/Footer.js
+++ ui/src/layouts/Footer.js
@@ -1,39 +1,11 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import Paper from '@mui/material/Paper';
+import React, { Fragment } from 'react'
+import '../App.css'
 
 const Footer = () => {
   return (
-    <Box mt='50px'>
-      <Paper sx={{ position: 'sticky', bottom: 0, left: 0, right: 0, mt: 'auto' }} elevation={3}>
-
-        <BottomNavigation>
-
-          <Grid sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', gap: '8px' }}>
-
-            <FitnessCenterIcon />
-
-            <Typography
-              sx={{
-                fontFamily: 'impact',
-                letterSpacing: '.1rem',
-                fontSize: '26px',
-              }}
-            >
-              WORKOUT BUD
-            </Typography>
-
-          </Grid>
-
-        </BottomNavigation>
-
-      </Paper>
-    </Box>
+    <Fragment>
+      Footer
+    </Fragment>
   );
 };
 
