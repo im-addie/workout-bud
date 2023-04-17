409aa2156abf9f31bba436212627372bee6744a0
Addie Pasok <addiepasok8@gmail.com>
2023-04-17 18:56:49 -0400

    added delete and register function

--- ui/src/context/userContext.js
+++ ui/src/context/userContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from 'react'
 import { isUserLoggedIn, getToken, setToken, clearToken } from '../utility/utils'
-import { login, getUser } from '../utility/api'
+import { register, login, getUser, deleteAccount } from '../utility/api'
 
 export const UserContext = createContext(null)
 
@@ -34,13 +34,35 @@ export const UserContextProvider = (props) => {
       .catch((error) => console.log(error))
   }
 
+  const createAccount = async (email, name, password) => {
+    const response = await register({
+      email,
+      name,
+      password
+    })
+
+    const token = response.token
+
+    setToken(token)
+    await getUser(token)
+      .then((data) => setUser(data)) // data.name gets the user's name
+      .catch((error) => console.log(error))
+  }
+
   const signOut = () => {
     setUser(null)
     clearToken()
   }
+
+  const removeAccount = async () => {
+    const token = getToken()
+    setUser(null)
+    await deleteAccount(token)
+    clearToken()
+  }
   
   return (
-    <UserContext.Provider value={{ user, setUser, signIn, signOut }}>
+    <UserContext.Provider value={{ user, setUser, signIn, signOut, createAccount, removeAccount }}>
       {children}
     </UserContext.Provider>
   )
