5765541791ae5344bd05f76c3083c274e7eff838
Addie Pasok <addiepasok8@gmail.com>
2023-04-17 18:35:04 -0400

    removed logout and added usercontextprovider

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,11 +3,11 @@ import { Routes, Route } from 'react-router-dom'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import ThemeContextProvider from './context/themeContext'
+import { UserContextProvider } from './context/userContext'
 import Layout from './layouts'
 import NotFound from './views/NotFound'
 import Home from './views/Home'
 import Login from './views/Login'
-import Logout from './views/Logout'
 import RegisterForm from './views/Register'
 import Settings from './views/Settings'
 import LogWorkout from './views/LogWorkout'
@@ -16,19 +16,20 @@ function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <LocalizationProvider dateAdapter={AdapterMoment}>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/register" element={<RegisterForm />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/log-workout" element={<LogWorkout />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </LocalizationProvider>
+        <UserContextProvider>
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<RegisterForm />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/log-workout" element={<LogWorkout />} />
+                <Route path="*" element={<NotFound />} />
+              </Route>
+            </Routes>
+          </LocalizationProvider>
+        </UserContextProvider>
       </ThemeContextProvider>
     </div>
   )
