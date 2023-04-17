d09d96363b20da27e2bd8b74d32f608d9856d260
Addie Pasok <addiepasok8@gmail.com>
2023-04-17 19:00:33 -0400

    FEAT: users can press enter to register; added userContext

--- ui/src/views/Register/index.js
+++ ui/src/views/Register/index.js
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom"
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Card, CardContent, Grid, Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
@@ -10,8 +10,7 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 import Button from '@mui/material/Button'
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
 import ListItem from '@mui/material/ListItem'
-import { register } from '../../utility/api'
-import { setToken } from '../../utility/utils'
+import { UserContext } from '../../context/userContext'
 
 function RegistrationForm() {
   const [emailValue, SetEmailValue] = useState("")
@@ -22,13 +21,15 @@ function RegistrationForm() {
   const [showPassword, setShowPassword] = useState(false)
   const [showRetypePassword, setShowRetypePassword] = useState(false)
 
+  const { createAccount } = useContext(UserContext)
+
   let navigate = useNavigate()
 
   const handleShowPassword = () => setShowPassword(!showPassword)
   const handleShowRetypePassword = () => setShowRetypePassword(!showRetypePassword)
 
-  const handleRegister = async () => {
-
+  const handleRegister = async (e) => {
+    e.preventDefault()
     try {
       // console.log('email value:', emailValue)
       // console.log('name value:', nameValue)
@@ -39,15 +40,7 @@ function RegistrationForm() {
         return setErrorMsg('Passwords do not match.')
       }
 
-      const tokenValue = await register({
-        email: emailValue,
-        name: nameValue,
-        password: passwordValue
-      })
-
-      // console.log('token value', tokenValue.token)
-
-      setToken(tokenValue.token)
+      await createAccount(emailValue, nameValue, passwordValue)
 
       routeToDashboard()
 
@@ -72,114 +65,117 @@ function RegistrationForm() {
     >
       <Card sx={{ borderRadius: '20px', boxShadow: '3px 2px 7px rgb(0, 0, 0, 0.5)', mt: '35px', width: '400px', height: '600px' }}>
 
-        <CardContent sx={{ display: 'grid', margin: '20px' }}>
+        <form onSubmit={handleRegister}>
+          <CardContent sx={{ display: 'grid', margin: '20px' }}>
 
-          <FitnessCenterIcon sx={{ display: 'flex', justifySelf: 'center' }} />
+            <FitnessCenterIcon sx={{ display: 'flex', justifySelf: 'center' }} />
 
-          <Typography
-            sx={{
-              fontFamily: 'impact',
-              fontWeight: 700,
-              letterSpacing: '.1rem',
-              fontSize: '26px',
-              color: 'inherit',
-              justifySelf: 'center'
-            }}
-          >
-            WORKOUT BUD
-          </Typography>
-
-          <Typography variant='h6' fontWeight='bold' mt='10px'>Register</Typography>
-
-          <Typography color='red' variant='caption'>{errorMsg}</Typography>
-
-          <Grid
-            container
-            direction="column"
-            justify="center"
-          >
-            <TextField
-              variant="filled"
-              label="Email"
-              sx={{ marginBottom: '15px', marginTop: '10px', }}
-              onChange={email => SetEmailValue(email.target.value)}
-              value={emailValue}
-            />
-
-            <TextField
-              variant="filled"
-              label="Name"
-              sx={{ marginBottom: '15px' }}
-              onChange={name => setNameValue(name.target.value)}
-              value={nameValue}
-            />
-
-            {/* password field */}
-            <TextField
-              variant="filled"
-              label="Password"
-              sx={{ marginBottom: '15px' }}
-              type={showPassword ? "text" : "password"} // if show password is true, the type turns to text. if not, its type is password
-              onChange={password => setPasswordValue(password.target.value)}
-              value={passwordValue}
-              InputProps={{ // toggle button
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleShowPassword}
-                    >
-                      {showPassword ? <VisibilityIcon fontSize='small' /> : <VisibilityOffIcon fontSize='small' />}
-                    </IconButton>
-                  </InputAdornment>
-                )
+            <Typography
+              sx={{
+                fontFamily: 'impact',
+                fontWeight: 700,
+                letterSpacing: '.1rem',
+                fontSize: '26px',
+                color: 'inherit',
+                justifySelf: 'center'
               }}
-            />
-
-            {/* retype password field */}
-            <TextField
-              variant="filled"
-              label="Re-type password"
-              sx={{ marginBottom: '5px' }}
-              type={showRetypePassword ? "text" : "password"} // if show password is true, the type turns to text. if not, its type is password
-              onChange={retypePassword => setRetypePasswordValue(retypePassword.target.value)}
-              value={retypePasswordValue}
-              InputProps={{ // toggle button
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleShowRetypePassword}
-                    >
-                      {showRetypePassword ? <VisibilityIcon fontSize='small' /> : <VisibilityOffIcon fontSize='small' />}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-
-            <Typography variant='caption'>Password requirements:
-              <ListItem disablePadding={true} sx={{ display: 'list-item', ml: '10px' }}>8 characters or longer.</ListItem>
-              <ListItem disablePadding={true} sx={{ display: 'list-item', ml: '10px' }}>Contain a number.</ListItem>
+            >
+              WORKOUT BUD
             </Typography>
 
-          </Grid>
+            <Typography variant='h6' fontWeight='bold' mt='10px'>Register</Typography>
 
-          <Box>
-            <Typography component={Link} to='/login' variant='caption' className='link'>
-              Already have an account? Log in here
-            </Typography>
-          </Box>
+            <Typography color='red' variant='caption'>{errorMsg}</Typography>
 
-          <Button
-            variant="contained"
-            sx={{ width: '90px', mt: '10px', display: 'flex', justifySelf: 'center' }}
-            onClick={() => handleRegister()}
-          >
-            Register
-          </Button>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+            >
+              <TextField
+                variant="filled"
+                label="Email"
+                sx={{ marginBottom: '15px', marginTop: '10px', }}
+                onChange={email => SetEmailValue(email.target.value)}
+                value={emailValue}
+              />
 
-        </CardContent>
+              <TextField
+                variant="filled"
+                label="Name"
+                sx={{ marginBottom: '15px' }}
+                onChange={name => setNameValue(name.target.value)}
+                value={nameValue}
+              />
+
+              {/* password field */}
+              <TextField
+                variant="filled"
+                label="Password"
+                sx={{ marginBottom: '15px' }}
+                type={showPassword ? "text" : "password"} // if show password is true, the type turns to text. if not, its type is password
+                onChange={password => setPasswordValue(password.target.value)}
+                value={passwordValue}
+                InputProps={{ // toggle button
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleShowPassword}
+                      >
+                        {showPassword ? <VisibilityIcon fontSize='small' /> : <VisibilityOffIcon fontSize='small' />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              />
+
+              {/* retype password field */}
+              <TextField
+                variant="filled"
+                label="Re-type password"
+                sx={{ marginBottom: '5px' }}
+                type={showRetypePassword ? "text" : "password"} // if show password is true, the type turns to text. if not, its type is password
+                onChange={retypePassword => setRetypePasswordValue(retypePassword.target.value)}
+                value={retypePasswordValue}
+                InputProps={{ // toggle button
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleShowRetypePassword}
+                      >
+                        {showRetypePassword ? <VisibilityIcon fontSize='small' /> : <VisibilityOffIcon fontSize='small' />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              />
+
+              <Typography variant='caption'>Password requirements:
+                <ListItem disablePadding={true} sx={{ display: 'list-item', ml: '10px' }}>8 characters or longer.</ListItem>
+                <ListItem disablePadding={true} sx={{ display: 'list-item', ml: '10px' }}>Contain a number.</ListItem>
+              </Typography>
+
+            </Grid>
+
+            <Box>
+              <Typography component={Link} to='/login' variant='caption' className='link'>
+                Already have an account? Log in here
+              </Typography>
+            </Box>
+
+            <Button
+              type='submit'
+              variant="contained"
+              sx={{ width: '90px', mt: '10px', display: 'flex', justifySelf: 'center' }}
+              onClick={() => handleRegister()}
+            >
+              Register
+            </Button>
+
+          </CardContent>
+        </form>
 
       </Card>
     </Grid>
